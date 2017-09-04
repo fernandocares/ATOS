0c8f97640ad2f294f4380e25757b1ef0b4a0344c
fernandocares <fernandocareswork@gmail.com>
2017-09-04 20:23:48 -0300

    asd

--- listar_usuarios.php
+++ listar_usuarios.php
@@ -2,7 +2,6 @@
 session_start();
 include_once("seguranca.php");
 include_once("conexao.php");
-include_once("destroi_dados.php");
 ?>
 
 <!DOCTYPE html>
--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+
+include_once("destroi_dados.php");
 ?>
 
 <!DOCTYPE html>
