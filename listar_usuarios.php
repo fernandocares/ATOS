6fcbbf44e1bb185ede24e59cac7e6a6e4f09c98e
fernandocares <fernandocareswork@gmail.com>
2017-09-18 20:26:31 -0300

    asd

--- listar_usuarios.php
+++ listar_usuarios.php
@@ -39,7 +39,6 @@ include_once("conexao.php");
   include_once("menu_admin.php");
   $resultado = mysql_query("SELECT * FROM usuarios ORDER BY 'id'");
   $linhas = mysql_num_rows($resultado);
-
 ?>
 
 <div class="container theme-showcase" role="main">
